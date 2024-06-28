230427fee1b2f1114923fd58677071d95083617a
Adi221199 <awarhade03@gmail.com>
2024-06-29 01:15:29 +0530

    kafka consumer service (End-User)

---
+++ src/main/java/com/enduser/config/KafkaConfig.java
@@ -0,0 +1,19 @@
+package com.enduser.config;
+
+import com.enduser.constants.AppConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@Configuration
+public class KafkaConfig {
+
+    private static final Logger log = LoggerFactory.getLogger(KafkaConfig.class);
+
+    @KafkaListener(topics = AppConstants.LOCATION_UPDATE_TOPIC, groupId = AppConstants.GROUP_ID)
+    public void updatedLocation(String value){
+
+        log.info("Location updated: {}", value);
+    }
+}
